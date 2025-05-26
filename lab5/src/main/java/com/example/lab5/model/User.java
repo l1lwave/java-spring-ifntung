3b9cb9db98a90cc55b70c46caa85a566d62b23bf
Putintsev_Kirill <135183716+l1lwave@users.noreply.github.com>
2025-05-26 21:55:59 +0300

    Add files via upload

---
+++ lab5/src/main/java/com/example/lab5/model/User.java
@@ -0,0 +1,25 @@
+package com.example.lab5.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
